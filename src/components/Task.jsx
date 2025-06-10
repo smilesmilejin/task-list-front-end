7411d2df58f7772749a81f44d302220ba024261c
Xinshuang Jin <jinxinshuang@gmail.com>
2025-06-10 14:30:20 -0400

    Completed Wave 1

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,14 +1,23 @@
 import PropTypes from 'prop-types';
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
+// const Task = ({ id, title, isComplete }) => {
+//   return (
+//     <li className="tasks__item">
+//       <button className="tasks__item__toggle">{title}</button>
+//       <button className="tasks__item__remove button">x</button>
+//     </li>
+//   );
+// };
+const Task = (props) => {
   return (
     <li className="tasks__item">
-      <button className="tasks__item__toggle">{title}</button>
+      <button className="tasks__item__toggle">{props.title}</button>
       <button className="tasks__item__remove button">x</button>
     </li>
   );
 };
+
 
 Task.propTypes = {
   id: PropTypes.number.isRequired,
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import Task from './Task.jsx';
 import './TaskList.css';
 
-const TaskList = ({ tasks }) => {
+/* const TaskList = ({ tasks }) => {
   const getTaskListJSX = (tasks) => {
     return tasks.map((task) => {
       return (
@@ -16,7 +16,22 @@ const TaskList = ({ tasks }) => {
     });
   };
   return <ul className="tasks__list no-bullet">{getTaskListJSX(tasks)}</ul>;
+}; */
+const TaskList = ({ tasks }) => {
+  return (
+    <ul className="tasks__list no-bullet">
+      {tasks.map((task) => (
+        <Task
+          key={task.id}
+          id={task.id}
+          title={task.title}
+          isComplete={task.isComplete}
+        />
+      ))}
+    </ul>
+  );
 };
+
 
 TaskList.propTypes = {
   tasks: PropTypes.arrayOf(
